587a15de30f5322a8e81ebfdea326bd3fc94381e
benser1 <briganenser28@gmail.com>
2016-03-26 07:37:48 -0700

    Show nested route set up

--- app/assets/javascripts/home/MainCtrl.js
+++ app/assets/javascripts/home/MainCtrl.js
@@ -1,19 +1,19 @@
  function MainCtrl($scope, workouts) {
   
-  $scope.workouts = workouts.workouts;
+  // $scope.workouts = workouts.workouts;
 
-  $scope.destroy = function(workout) {
-      workouts.destroy(workout);
-  };
+  // $scope.destroy = function(workout) {
+  //     workouts.destroy(workout);
+  // };
 
-  $scope.addWorkout = function() {
-    if(!$scope.title || $scope.title === '') { return; }
+  // $scope.addWorkout = function() {
+  //   if(!$scope.title || $scope.title === '') { return; }
 
-    workouts.create({
-      title: $scope.title
-    });
-    $scope.title = '';
-  };
+  //   workouts.create({
+  //     title: $scope.title
+  //   });
+  //   $scope.title = '';
+  // };
 }
 
  angular 
